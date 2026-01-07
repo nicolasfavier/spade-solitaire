c65db1801014dfa26e9a06081151b77e82a1a48e
nicolasfavier <niko.favier@gmail.com>
2026-01-07 14:02:09 +0100

    Fix routing for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/spade-solitaire/',
+  base: mode === 'production' ? '/spade-solitaire/' : '/',
   server: {
     host: "::",
     port: 8080,
